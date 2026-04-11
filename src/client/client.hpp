cc716a72531307911d2e36e4d0c54735205685e2
mezleca <mtksenpai@gmail.com>
2026-04-10 22:35:08 -0300

    formatt

--- src/client/client.hpp
+++ src/client/client.hpp
@@ -62,8 +62,8 @@ struct OsuBeatmap {
           spinners(b.spinners), last_modification_time(b.last_modification_time), approach_rate(b.approach_rate),
           circle_size(b.circle_size), hp_drain(b.hp_drain), overall_difficulty(b.overall_difficulty), tags(b.tags),
           slider_velocity(b.slider_velocity), drain_time(b.drain_time), total_time(b.total_time), duration(b.duration),
-          audio_preview_time(b.audio_preview_time), difficulty_id(b.difficulty_id), beatmap_id(b.beatmap_id), source(b.source),
-          mode((Gamemode)b.mode) {
+          audio_preview_time(b.audio_preview_time), difficulty_id(b.difficulty_id), beatmap_id(b.beatmap_id),
+          source(b.source), mode((Gamemode)b.mode) {
     }
     std::string searchable;
     std::string artist_unicode;
@@ -89,12 +89,9 @@ struct OsuBeatmap {
     int beatmap_id = 0;
 
     void build_search() {
-        searchable = boost::locale::to_lower(
-            fmt::format("{} {} {} {} {} {} {} {} {} {}", 
-                title, title_unicode, artist, artist_unicode, creator, difficulty, source, tags, 
-                difficulty_id, beatmap_id
-            )
-        );
+        searchable = boost::locale::to_lower(fmt::format("{} {} {} {} {} {} {} {} {} {}", title, title_unicode, artist,
+                                                         artist_unicode, creator, difficulty, source, tags,
+                                                         difficulty_id, beatmap_id));
 
         using nm = boost::locale::norm_type;
         searchable = boost::locale::normalize(searchable, nm::norm_nfd);
